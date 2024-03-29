ad9ff8f7cf2627af59402760a6a0cd8270ec5c56
teo1962 <teo1962@yandex.ru>
2024-03-29 09:07:18 +0600

    package change

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "LuaxStdlib",
-            targets: ["LuaxStdlib"]),
+            name: "LuaxStdlib-Swift",
+            targets: ["LuaxStdlib-Swift"]),
     ],
      dependencies: [
          // Dependencies declare other packages that this package depends on.
@@ -23,7 +23,7 @@ let package = Package(
          // Targets are the basic building blocks of a package. A target can define a module or a test suite.
          // Targets can depend on other targets in this package, and on products in packages this package depends on.
          .target(
-             name: "LuaxStdlib",
+             name: "LuaxStdlib-Swift",
              dependencies: [
                  "SwiftyXMLParser",
                  "DataDeflate",
